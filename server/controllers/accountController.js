ed589c2f49fe4a4bdad09a125304079d0a4eb9bc
Luuk_ <74959580+luuk0406@users.noreply.github.com>
2022-09-27 14:26:08 +0200

    added signup endpoint (#1)
    * added signup endpoint
    
    * Update .env
    
    oldass
    
    * Update accountController.js
    
    Co-authored-by: luuk0406 <luuk@byron.nl>
    Co-authored-by: fabiantax87 <74959529+fabiantax87@users.noreply.github.com>

--- server/controllers/accountController.js
+++ server/controllers/accountController.js
@@ -15,3 +15,24 @@ exports.getAccount = async (req, res) => {
     res.status(400).json({ message: err });
   }
 };
+
+/**
+ * /api/account/signup/{username}
+ * POST create new account
+ */
+
+exports.createAccount = async (req, res) => {
+   const newAccount = new Account({
+      username: req.body.username
+      // expand fields <<
+      // check if data doesn't exsist yet
+   });
+  
+   try {
+      await newAccount.save();
+      res.json(newAccount);
+   }catch(err) {
+      console.log(err.message);
+      res.status(400).json({ message: err });
+   }
+}
--- server/routes/accountRoutes.js
+++ server/routes/accountRoutes.js
@@ -4,4 +4,6 @@ const accountController = require("../controllers/accountController");
 
 router.get("/api/account/:id", accountController.getAccount);
 
+router.post("/api/account/signup", accountController.createAccount);
+
 module.exports = router;
