ce2a3512abf4f7a9bf175013a555aac17ff46234
Yigit Findik <yigit8@live.nl>
2022-10-10 14:47:19 +0200

    levels/game_id endpoint

--- server/controllers/levelController.js
+++ server/controllers/levelController.js
@@ -16,3 +16,27 @@ exports.getLevels = async (req, res) => {
         })
     }
 }
+
+/**
+ * /api/levels/:game_id
+ * GET all levels of the game
+ */
+
+exports.getGameLevels = async (req, res) => {
+
+    //!create query to retrieve all levels of a game by game_id
+    //!could also use find and then query instead of whats below at line 34
+    //!need approval of davor or anyone else that has knowledge of this
+    const query = {
+        game_id: req.params.game_id
+    }
+
+    try {
+        const levels = await Level.find({game_id: req.params.game_id});
+        res.json(levels);
+    } catch (err) {
+        res.status(400).json({
+            message: err
+        })
+    }
+}
