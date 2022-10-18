61baf43848205eafda19be0b9a51846f081502f0
valentijnoa <74962322+valentijnoa@users.noreply.github.com>
2022-10-18 14:15:07 +0200

    Merge branch 'main' of

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
@@ -8,14 +9,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("dist"));
 
 const userRoutes = require("./server/routes/userRoutes.js");
 const levelRoutes = require("./server/routes/levelRoutes.js");
-const gameRoutes = require("./server/routes/gameRoutes.js");
-app.use("/", userRoutes, levelRoutes);
+app.use("/", userRoutes, levelRoutes, gameRoutes);
 
 app.get("/", (req, res) => {
-  res.send("technovium unlimited api");
+  res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
