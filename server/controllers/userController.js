d6daf12d425375e546d6e359341d781d375227a3
assadmustafa <0168379@student.roc-nijmegen.nl>
2022-10-31 14:16:16 +0100

    Initial Setup

---
+++ server/controllers/userController.js
@@ -0,0 +1,92 @@
+require("../models/db");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken')
+let refreshTokens = []
+
+/**
+ * /api/user/{id}
+ * GET user based on id
+ */
+
+exports.getUser = async (req, res) => {
+  let paramID = req.params.id;
+  try {
+    const user = await User.findOne({ _id: paramID });
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
+};
+
+/** 
+ * /api/user/all
+ * GET all users returned
+*/
+
+exports.getAllUsers = async (req, res) => {
+
+   try {
+      const users = await User.find({});
+      res.json(users);
+
+   } catch (err) {
+      res.status(400).json({message: err});
+   }
+}
+
+/**
+ * /api/user/signup/{username}
+ * POST create new user
+ */
+
+exports.createUser = async (req, res) => {
+
+   const hashedPass = await bcrypt.hash(req.body.password, 10);
+
+  const newUser = new User({
+   id: req.body.id,
+   username: req.body.username,
+   email: req.body.email,
+   password: hashedPass,
+   date_of_birth: req.body.date_of_birth,
+   gender: req.body.gender,
+   status: req.body.status
+  });
+
+  try {
+    await newUser.save();
+    res.json(newUser);
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).json({ message: err });
+  }
+};
+
+/**
+ * /api/user/login/{username}
+ * POST login user
+ */
+
+exports.loginUser = async (req, res) => {
+  const user = User({
+      id: req.body.id,
+      email: req.body.email,
+      password: req.body.password
+    });
+
+    const accessToken = generateAccessToken(user)
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+
+  try {
+      refreshTokens.push(refreshToken)
+      res.json({user, accessToken: accessToken, refreshToken: refreshToken})
+  } catch (err) {
+   console.log(err.message);
+   res.status(400).json({ message: err });
+  }
+};
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' })
+}
---
+++ server/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date_of_birth: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Users", userSchema);
